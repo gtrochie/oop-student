cf55fc8953e21a7a2426ce6b91ee50aa067369e6
Rotshidzwa Nematswerani <gtrochie@gmail.com>
2026-05-11 12:22:33 +0200

    done with constructors

--- src/main/java/za/co/gtrochie/Student/Student.java
+++ src/main/java/za/co/gtrochie/Student/Student.java
@@ -1,4 +1,26 @@
 package za.co.gtrochie.Student;
 
 public class Student {
+    private String name;
+    private int studentNumber;
+
+    public Student(String name){
+        if (name == null){
+            throw new NullPointerException("name may not be null");
+        }
+        if (name.isEmpty()){
+            throw new IllegalArgumentException("name may not be empty");
+        }
+        this.name = name;
+    }
+
+    public Student(String name, int studentNumber){
+        this.name = name;
+        if (studentNumber <= 0){
+            throw new IllegalArgumentException("student number must be a positive integer")
+        }
+        this.studentNumber = studentNumber;
+    }
+
+
 }
